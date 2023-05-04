925428383989922ad6f449efe73e67beb1e5b68d
Lucão <lukaspc2019@gmail.com>
2023-05-03 21:31:48 -0300

    Tratamento de exceções

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,11 @@
+const AppError = require("../utils/AppError");
 class UsersController {
   create(request, response){
     const { name, email, password } = request.body;
 
+    if(!name){
+      throw new AppError("Nome Obrigatório!!!")
+    }
     response.status(201).json({name, email, password});
   }
 }

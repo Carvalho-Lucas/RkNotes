9796c73f830523c018d12d9f228c3a754d3537e9
Lucão <lukaspc2019@gmail.com>
2023-05-10 15:36:21 -0300

    Utilização de Token para acesso na API

--- Back-End/src/controllers/SessionsController.js
+++ Back-End/src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
 const { compare } = require('bcryptjs')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
 
 class SessionsController{
   async create(request, response){
@@ -19,7 +21,14 @@ class SessionsController{
       throw new AppError("E-mail e/ou Senha Incorreto!", 401)
     }
 
-    return  response.json(user)
+    //JWT sign
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+    return response.json({user, token})
   }
 }
 
--- Back-End/src/controllers/UsersController.js
+++ Back-End/src/controllers/UsersController.js
@@ -25,10 +25,10 @@ class UsersController {
 
   async update(request, response){
     const { name, email, password, old_password} = request.body;
-    const { id } = request.params;
+    const user_id = request.user.id
   
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id] ); //seleciona todos os campos da tabela usuario onde o id seja igual ao [id] 
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id] ); //seleciona todos os campos da tabela usuario onde o id seja igual ao [id] 
     
     if (!user) {
       throw new AppError('Usuário não encontrado!');
@@ -67,7 +67,7 @@ class UsersController {
     password = ?,
     updated_at = DATETIME('now')
     WHERE id = ?`,
-    [user.name, user.email, user.password, id ]);
+    [user.name, user.email, user.password, user_id ]);
   
     return response.json();
   }
